78a642c9006a80d36ba64addd335ab9b64cd0ab7
Alex Dieguez <adieguezisc@gmail.com>
2018-10-17 10:14:45 -0500

    exercise solved

--- Second Section/Start Dinamic Components/src/App.vue
+++ Second Section/Start Dinamic Components/src/App.vue
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button @click="componentId = 'app-blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="componentId = 'app-green'" class="btn btn-success">Load Green Template</button>
+                <button @click="componentId = 'app-red'" class="btn btn-danger">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="componentId">
+                    <h1>Hola Mundo</h1>
+                </component>
             </div>
         </div>
     </div>
@@ -21,6 +21,11 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function() {
+            return {
+                componentId: 'app-blue'
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
